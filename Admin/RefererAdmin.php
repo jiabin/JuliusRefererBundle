387c3effc583a512b8c49e9e97933d05c42b9caa
Eymen Gunay <eymen@egunay.com>
2014-02-27 20:49:17 +0100

    Base sonata admin class fixed

--- Admin/RefererAdmin.php
+++ Admin/RefererAdmin.php
@@ -2,7 +2,7 @@
 
 namespace Julius\RefererBundle\Admin;
 
-use Julius\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Validator\ErrorElement;
